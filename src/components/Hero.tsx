47ae87e741feae48272f9e146b2a133ac62eab41
rajaabbas2002 <abbasaffandi2002@gmail.com>
2023-10-12 07:17:44 -0700

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroNav from "./common/Header"
+
+
+function Hero() {
+  return (
+    <div className='Hero'>
+      <HeroNav/>
+    </div>
+  )
+}
+
+export default Hero
