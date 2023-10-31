843cdd6d9c9ae094c4f26abb5f9c47a1000f6597
rajaabbas2002 <abbasaffandi2002@gmail.com>
2023-10-31 12:10:21 -0700

    property-app

--- src/components/Home/Video.tsx
+++ src/components/Home/Video.tsx
@@ -11,21 +11,21 @@ function Video() {
     <p className="text-center text-black col-6 opacity-50 fs-1 font-family-Poppins  m-0 px-3 py-2">
     Explore The Beautiful Jersey Shore
     </p>
-</div>    <div className=" row align-items-center d-flex">
+</div>    <div className="row align-items-center d-flex">
         <div className="px-1 py-5 bg-opacity-50 justify-content-center align-items-center d-flex">
             <div className="position-relative shadow mx-2">
-            <img src={Video1} className="img-fluid" alt="Responsive image" loading='lazy'/>
+            <img src={Video1} className="img-fluid imagebutton" alt="Responsive image" loading='lazy'/>
             </div>
         </div>
         <div className="justify-content-center d-flex flex-sm-nowrap gap-5">
             <div className="bg-opacity-50 justify-content-center align-items-center video">
                 <div className="position-relative shadow d-flex justify-content-center">
-                <img src={Video2} alt="..." className="img-fluid" loading='lazy'/>
+                <img src={Video2} alt="Video2" className="img-fluid imagebutton" loading='lazy'/>
                 </div>
             </div>
             <div className="bg-opacity-50 justify-content-center align-items-center video">
                 <div className="position-relative shadow d-flex justify-content-center">
-                <img src={Video3} alt="Video3" className="img-fluid" loading='lazy'/>
+                <img src={Video3} alt="Video3" className="img-fluid imagebutton" loading='lazy'/>
                 </div>
             </div>
         </div>
